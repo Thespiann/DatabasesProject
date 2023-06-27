16cd3160903acfc9db20ec07919cefb651ece975
YannisAba <97945141+YannisAba@users.noreply.github.com>
2023-06-27 14:23:26 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -6,3 +6,10 @@ JOIN match ON (team.name=match.home_team )--ή θα μπορούσε να είν
 WHERE match.id=3
 
 --Ερώτημα 2β
+SELECT game_event.event_type, game_event.event_time, player.name AS player_name
+FROM game_event
+JOIN player ON game_event.player_id = player.id
+JOIN match on game_event.match_id = match.id
+WHERE match.id = '5' AND (game_event.event_type = 'goal' OR game_event.event_type = 'penalty kick');
+
+--Ερώτημα 2γ
