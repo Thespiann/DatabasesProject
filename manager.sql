dd6b2557ad54a998bb20c77b061a676bf6a42597
Thespiann <100039436+Thespiann@users.noreply.github.com>
2023-06-27 12:56:12 +0300

    Update manager.sql

--- manager.sql
+++ manager.sql
@@ -4,3 +4,26 @@ insert into manager (name, last_name, team, past_position, total_minutes) values
 insert into manager (name, last_name, team, past_position, total_minutes) values ('Ιωσήφ', 'Γιαλαμάς', 'Olympiacos', 'Center Back', 13727);
 insert into manager (name, last_name, team, past_position, total_minutes) values ('Δήμος', 'Κεχαγιάς', 'Aris', 'Attacking Midfielder', 28215);
 insert into manager (name, last_name, team, past_position, total_minutes) values ('Κλεάνθης', 'Φωτόπουλος', 'Volos', 'Right Back', 22881);
+
+--function to insert manager using user:
+CREATE OR REPLACE FUNCTION promote_to_manager(player_id INT)
+RETURNS VOID AS $$
+DECLARE
+    total_minutes INTEGER;
+BEGIN
+    -- Calculate the total_minutes
+    SELECT COALESCE(SUM(duration), 0)
+    INTO total_minutes
+    FROM minutes_per_match
+    WHERE player_id = player_id;
+
+    -- Insert into the manager table
+    INSERT INTO manager (name, last_name, team, past_position, total_minutes)
+    SELECT name, last_name, team, player_position, total_minutes
+    FROM player
+    WHERE id = player_id;
+
+    -- Delete from the player table
+    DELETE FROM player WHERE id = player_id;
+END;
+$$ LANGUAGE plpgsql;
